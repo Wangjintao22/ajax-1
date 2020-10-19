442508495e710c520a83565173aea67469946273
frankwang <chacha1351513435@gmail.com>
2020-10-19 12:45:21 +0800

    完成

---
+++ public/main.js
@@ -0,0 +1,95 @@
+console.log("1")
+let n = 1
+getPage.onclick=()=>{
+    const request=new XMLHttpRequest()
+    request.open('GET',`./page${n+1}`)
+    request.onreadystatechange=()=>{
+        if(request.readyState===4 && request.status>=200 && request.status<300){
+            const array = JSON.parse(request.response)
+            array.forEach(item=>{
+                const li = document.createElement('li')
+                li.textContent = item.id
+                xxx.appendChild(li)
+            })
+            n+=1
+        }
+    }
+    request.send()
+}
+getJSON.onclick=()=>{
+    const request=new XMLHttpRequest()
+    request.open('GET','./5.json')
+    request.onreadystatechange=()=>{
+        if(request.readyState===4 && request.status>=200 && request.status<300){
+            console.log(request.response)
+            const object = JSON.parse(request.response)
+            console.log(object)
+        }
+    }
+    request.send()
+}
+getXML.onclick=()=>{
+    const request=new XMLHttpRequest()
+    request.open('GET','./4.xml')
+    request.onreadystatechange=()=>{
+        if(request.readyState===4 && request.status>=200 && request.status<300){
+            const dom = request.responseXML
+            const text = dom.getElementsByTagName('warning')[0].textContent
+            console.log(text.trim())
+        }
+    }
+    request.send()
+}
+getHTML.onclick=()=>{
+    const request=new XMLHttpRequest()
+    request.open('GET','./3.html')
+    request.onload=()=>{
+        const div = document.createElement('div')
+        div.innerHTML = request.response
+        document.body.appendChild(div)
+    }
+    request.onerror=()=>{
+
+    }
+    request.send()
+}
+
+getJS.onclick=()=>{
+    const request=new XMLHttpRequest()
+    request.open('GET','./2.js')
+    request.onload=()=>{
+        const script=document.createElement('script')
+        script.innerHTML=request.response
+        document.body.appendChild(script)
+    }
+    request.onerror=()=>{
+
+    }
+    request.send()
+}
+getCSS.onclick=()=>{
+    const request=new XMLHttpRequest()
+    request.open('GET','./style.css')
+    request.onreadystatechange=()=>{
+        if(request.readyState===4){
+            if(request.status>=200 && request.status<300){
+            const style=document.createElement('style')
+            style.innerHTML=request.response
+            document.head.appendChild(style)
+            }else{
+                alert('加载CSS失败')
+            }
+        }      
+    }
+    // request.onload=()=>{
+    //     console.log('request.response')
+    //     console.log(request.response)
+    //     const style=document.createElement('style')
+    //     style.innerHTML=request.response
+    //     document.head.appendChild(style)
+    // }
+    // request.onerror=()=>{
+    //     console.log('失败了')
+    // }
+    request.send()
+}
